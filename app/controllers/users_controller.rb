b45dd21de65ad079c3b51a705456a08ea2f9ef8e
unoteppei <pasuta46@icloud.com>
2023-08-29 17:33:08 +0900

    プロフィール画面作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def character; end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = current_user
+    if @user.update_attributes(current_user_params)
+      flash[:notice] = '保存しました'
+    else
+      flash[:alert] = '更新できません'
+    end
+    redirect_to character_path
+  end
+
+  private
+
+  def current_user_params
+    params.permit(:about, :status, :avatar)
+  end
+end
