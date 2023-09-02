b3881a209b972803db831ffbf71fed31de15a76b
unoteppei <pasuta46@icloud.com>
2023-09-02 15:02:39 +0900

    削除機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @posts = Post.select("id")
     @posts = current_user.posts.all.order(created_at: :desc)
   end
 
@@ -28,7 +29,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def edit
     @post = Post.find(params[:id])
   end
