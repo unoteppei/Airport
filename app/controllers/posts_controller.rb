877b87453dac8a46efc657c7b69ff94ad5de7947
unoteppei <pasuta46@icloud.com>
2023-09-10 09:08:59 +0900

    ログインしていなくても使用可能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:map]
 
   def index
     @posts = Post.select("id")
