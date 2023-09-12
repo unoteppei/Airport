85711fb284ccfaa9c26cd279d6efeab1585d52da
unoteppei <pasuta46@icloud.com>
2023-09-12 11:08:58 +0900

    複数マーカー設置

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,9 +27,8 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
-  
+
   def edit
     @post = Post.find(params[:id])
   end
@@ -54,6 +53,7 @@ class PostsController < ApplicationController
   end
 
   def map
+    @posts = Post.all
   end
 
   def search
