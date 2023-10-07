c950e4812c00976a507ef6b3ad743596ed49b746
unoteppei <pasuta46@icloud.com>
2023-10-07 10:23:36 +0900

    バリデーション コメント投稿設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    if @comment.save
+      flash[:notice] = 'コメントを投稿しました'
+      redirect_to post_path
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :post_id)
+  end
 end
