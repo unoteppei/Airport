24b0b097c028ba9a20ca52730ca34b9d2ab97951
unoteppei <pasuta46@icloud.com>
2023-10-09 09:41:34 +0900

    不要なファイル削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: 'posts#map'
 
   devise_for :users,
-             path_names: { sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout' },
-             controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
+              path_names: { sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout' },
+              controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
