0fd279719be9d46aad26f139322451ef91152ab3
unoteppei <pasuta46@icloud.com>
2023-10-15 08:40:42 +0900

    空港新規投稿テスト追加

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,8 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   let(:user) {create(:user)}
   let(:post) { create(:post) }
+
+  describe "GET /new" do
+    it "空港投稿画面に成功すること" do
+      sign_in user
+      get new_post_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
   describe 'GET/posts/index' do
-    it '空港投稿一覧表示に成功すること' do
+    it '空港投稿一覧に成功すること' do
       get posts_path
       expect(response).to have_http_status(302)
     end
